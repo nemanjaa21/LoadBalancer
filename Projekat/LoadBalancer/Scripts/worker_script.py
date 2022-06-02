066cefbaa471fe17607e1ca875ff910c44519c49
nemanjaASE <nightwalker44ASE@gmail.com>
2022-06-02 22:59:53 +0200

    Dodavanje i uklanjanje workera
    Implementacija new_worker i terminate_worker unutar worker_script.py.

--- Projekat/LoadBalancer/Scripts/worker_script.py
+++ Projekat/LoadBalancer/Scripts/worker_script.py
@@ -5,9 +5,23 @@ import threading
 class Worker_script:
 
     def new_worker(self, conn, number_of_workers, workers):
-        #TO DO
-        pass
+        number_of_workers = number_of_workers + 1
+        _worker = Worker(f'WORKER_{number_of_workers - 1}')
+        t = threading.Thread(target=_worker.start_worker, name=f'WORKER_{number_of_workers - 1}')
+        print(f'[NEW WORKER] client created new worker!')
+        t.start()
+
+        workers.append(_worker)
+        _result = f"WORKER_{number_of_workers - 1} uspesno dodat!"
+        conn.send(_result.encode('utf-8'))
+        return number_of_workers
 
     def terminate_worker(self, name, workers, number_of_workers):
-        #TO DO
-        pass
+        if name != "WORKER_0":
+            for _worker in workers:
+                if _worker.name == name:
+                    _worker.queue_put("!TERM")
+                    workers.remove(_worker)
+                    print("[{name}] Terminated".format(name=_worker.name))
+                    number_of_workers = number_of_workers - 1
+        return number_of_workers

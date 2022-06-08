4e89c52ca568406de1c2efc8468400f1cc2dc029
nemanjaa21 <nemanja_malinovic@yahoo.com>
2022-06-08 21:05:30 +0200

    Analitics report

---
+++ Projekat/DatabaseCRUD/AnaliticsReport.py
@@ -0,0 +1,46 @@
+from mysql.connector import connect, Error
+
+
+class AnaliticsReport:
+
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
+    def potrosnja_po_gradu(self, *args):
+        _grad = args[0]
+        try:
+            with connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database
+            ) as connecting:
+                query = """SELECT pb.IdBrojila, Mesec, Potrosnja FROM brojilo p, potrosnjaBrojila pb 
+                           WHERE p.IdBrojila = pb.IdBrojila and Grad = (?) ORDER BY pb.IdBrojila, Mesec;"""
+                with connecting.cursor(prepared=True) as cursor:
+                    cursor.execute(query, (_grad,))
+                    result = cursor.fetchall()
+                    return result
+        except Error as e:
+            return e
+
+    def potrosnja_po_brojilu(self, *args):
+        _id = args[0]
+        try:
+            with connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database
+            ) as connecting:
+                query = """SELECT Mesec, Potrosnja FROM potrosnjaBrojila 
+                           WHERE IdBrojila = (?) ORDER BY Mesec;"""
+                with connecting.cursor(prepared=True) as cursor:
+                    cursor.execute(query, (_id,))
+                    result = cursor.fetchall()
+                    return result
+        except Error as e:
+            return e
--- Projekat/DatabaseCRUD/databaseCRUD.py
+++ Projekat/DatabaseCRUD/databaseCRUD.py
@@ -1,5 +1,6 @@
 from Projekat.DatabaseCRUD.CRUDBrojila import CRUDBrojila
 from Projekat.DatabaseCRUD.CRUDPotrosnjaBrojila import CrudPotrosnjaBrojila
+from Projekat.DatabaseCRUD.AnaliticsReport import AnaliticsReport
 
 
 class DatabaseCRUD:
@@ -7,6 +8,8 @@ class DatabaseCRUD:
     def __init__(self):
         self.crud_brojila = CRUDBrojila('127.0.0.1', 'admin', 'admin', 'RES_PROJEKAT')
         self.crud_potrosnja_brojila = CrudPotrosnjaBrojila('127.0.0.1', 'admin', 'admin', 'RES_PROJEKAT')
+        self.analitics = AnaliticsReport('127.0.0.1', 'admin', 'admin', 'RES_PROJEKAT')
+
 
     def get_brojilo(self, id_param):
         return self.crud_brojila.read(id_param)
@@ -17,3 +20,8 @@ class DatabaseCRUD:
     def insert(self, id_param, mesec_param, potrosnja_param):
         return self.crud_potrosnja_brojila.insert(id_param, mesec_param, potrosnja_param)
 
+    def get_potrosnja_po_gradu(self, grad_param):
+        return self.analitics.potrosnja_po_gradu(grad_param)
+
+    def get_potrosnja_po_brojilu(self, id_param):
+        return self.analitics.potrosnja_po_brojilu(id_param)

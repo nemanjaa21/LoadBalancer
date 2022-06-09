ba7f5cf1be80aeb0a1d984ec1ede02b6049e85f2
nemanjaASE <nightwalker44ASE@gmail.com>
2022-06-09 22:33:04 +0200

    MockDB
    Kreiranje klase MockDB koja implementira setUpClass i tearDownClass metode nasleđene TestCase klase. Namena MockDB je da omogući testiranje CRUD metoda tako da se ne koriste stvarni podaci iz baze.

---
+++ Projekat/Tests/MockDB.py
@@ -0,0 +1,133 @@
+from unittest import TestCase
+import mysql.connector
+from mysql.connector import errorcode
+from Projekat.DatabaseCRUD.CRUDBrojila import CRUDBrojila
+from Projekat.DatabaseCRUD.CRUDPotrosnjaBrojila import CrudPotrosnjaBrojila
+
+MYSQL_USER = "admin"
+MYSQL_PASSWORD = "admin"
+MYSQL_DB = "test"
+MYSQL_HOST = "127.0.0.1"
+
+
+class MockDB(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cnx = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD,
+        )
+        cursor = cnx.cursor(dictionary=True)
+
+        # drop database if it already exists
+        try:
+            cursor.execute("DROP DATABASE {}".format(MYSQL_DB))
+            cursor.close()
+            print("DB dropped")
+        except mysql.connector.Error as err:
+            print("{}{}".format(MYSQL_DB, err))
+
+        cursor = cnx.cursor(dictionary=True)
+        try:
+            cursor.execute(
+                "CREATE DATABASE {}".format(MYSQL_DB))
+        except mysql.connector.Error as err:
+            print("Failed creating database: {}".format(err))
+            exit(1)
+        cnx.database = MYSQL_DB
+
+        query = """
+                  CREATE TABLE `brojilo` (
+                  `IdBrojila` INTEGER NOT NULL,
+                  `Ime` VARCHAR(35) NOT NULL,
+                  `Prezime` VARCHAR(35) NOT NULL,
+                  `Ulica` VARCHAR(35) NOT NULL,
+                  `Broj` INTEGER NOT NULL,
+                  `PostanskiBroj` CHAR(5) NOT NULL,
+                  `Grad` VARCHAR(30) NOT NULL,
+                   CONSTRAINT `brojilo_PK` PRIMARY KEY (`IdBrojila`),
+                   CONSTRAINT `brojilo_CK` CHECK (REGEXP_LIKE(`PostanskiBroj`, '[0-9]{5}')))
+                """
+        try:
+            cursor.execute(query)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("'Brojilo' already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+        insert_data_query = """
+                               INSERT INTO `brojilo` (`IdBrojila`,`Ime`,`Prezime`,`Ulica`,`Broj`, `PostanskiBroj`, `Grad`)
+                               VALUES (1,"Janko","Jankovic","Vuka Karadzica",50,"21101", "Novi Sad"),
+                                      (3,"Marko","Markovic","Karadjordjeva",11,"11000", "Beograd"),
+                                      (7,"Zoran","Zoranovic","Politova",13,"21480", "Srbobran")
+                            """
+        cls.db1 = CRUDBrojila("127.0.0.1", "admin", "admin", "test")
+
+        try:
+            cursor.execute(insert_data_query)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            print("Data insertion to 'Brojilo' failed \n" + err)
+
+        query = """
+                        CREATE TABLE `potrosnjaBrojila`(
+                        `IdBrojila` INTEGER NOT NULL,
+                        `Potrosnja` DOUBLE NOT NULL,
+                        `Mesec` INTEGER NOT NULL,
+                         CONSTRAINT `potrosnjaBrojila_PK` PRIMARY KEY (`IdBrojila`, `Mesec`),
+                         CONSTRAINT `potrosnjaBrojila_FK` FOREIGN KEY (`IdBrojila`) REFERENCES brojilo(`IdBrojila`),
+                         CONSTRAINT `potrosnjaBrojila_CK1` CHECK (`Mesec` > 0 and `Mesec` <= 12),
+                         CONSTRAINT `potrosnjaBrojila_CK2` CHECK (`Potrosnja` >= 0)) 
+                       """
+
+        try:
+            cursor.execute(query)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("'potrosnjaBrojila' already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+        insert_data_query = """
+                                INSERT INTO `potrosnjaBrojila` (`IdBrojila`,`Mesec`,`Potrosnja`)
+                                VALUES (1, 2, 1000),
+                                       (1, 3, 999),
+                                       (3, 4, 555)
+                                    """
+        cls.db2 = CrudPotrosnjaBrojila("127.0.0.1", "admin", "admin", "test")
+
+        try:
+            cursor.execute(insert_data_query)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            print("Data insertion to 'potrosnjaBrojila' failed \n" + err)
+
+        cursor.close()
+        cnx.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        cnx = mysql.connector.connect(
+            host=MYSQL_HOST,
+            user=MYSQL_USER,
+            password=MYSQL_PASSWORD
+        )
+        cursor = cnx.cursor(dictionary=True)
+
+        # drop test database
+        try:
+            cursor.execute("DROP DATABASE {}".format(MYSQL_DB))
+            cnx.commit()
+            cursor.close()
+        except mysql.connector.Error as err:
+            print("Database {} does not exists. Dropping db failed".format(MYSQL_DB))
+        cnx.close()
